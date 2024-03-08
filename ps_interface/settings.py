ea21f9c57974ae8eaaf359961a69c4bbe2ce03be
jtai <jtai@jtais-laptop.drake.edu>
2024-03-08 14:45:26 -0600

    remove unnecessary static files configuration

--- ps_interface/settings.py
+++ ps_interface/settings.py
@@ -132,13 +132,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
-    BASE_DIR / 'ps_harvester/static',
-    BASE_DIR / 'ps_auth/static',
-    BASE_DIR / 'ps_users/static',
 ]
 
 
